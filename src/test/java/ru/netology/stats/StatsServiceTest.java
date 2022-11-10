64c58eb1a416e85198ce5635a9dcbbb2e6bb9eb1
Dulcev Dmitriy <ddvvvvvv@yandex.ru>
2022-11-10 21:25:49 +0300

    statsService

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -1,2 +1,41 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
 public class StatsServiceTest {
-}
+
+    @Test
+    public void ShouldSum() {
+        long sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        StatsService service = new StatsService();
+
+        long actual = service.sum(sales);
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    {
+        @Test
+        public void ShouldSum () {
+        long sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        StatsService service = new StatsService();
+
+        long actual = service.sum(sales);
+        long expected = 180 / 12;
+        Assertions.assertEquals(expected, actual)
+    }
+    }
+
+
+    @Test
+    public void ShouldSum() {
+        long sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        StatsService service = new StatsService();
+
+        int actual = service.belowAverege(sales);
+        long expected = 15;
+        Assertions.assertEquals(expected, actual);
+
+    }
