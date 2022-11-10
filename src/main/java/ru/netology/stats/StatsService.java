b8ea1bbf0ef38c0b40c37ba6249dd3bf22df6821
Dulcev Dmitriy <ddvvvvvv@yandex.ru>
2022-11-10 21:25:49 +0300

    statsService

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -1,4 +1,29 @@
-package 
+package ru.netology.stats;
+
+import java.util.Arrays;
 
 public class StatsService {
+   public long sum(long[] sales){
+       long sum =0;
+       for (long sale : sales) {
+           sum += sale;
+           
+       }
+       return sum;
+   }
+
+   public long average(long [] sales) {
+       return sum(sales) /12;
+   }
+
+   public int belowAverege(long[] sales){
+       long avg = average(sales);
+       int count=0;
+       for (long sale : sales) {
+           if (sale <avg){
+               count++;
+           }
+       }
+       return count;
+   }
 }
