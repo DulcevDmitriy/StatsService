6f7cef4eecc7b97d5713313b84eb858d5b4e6d99
Dulcev Dmitriy <ddvvvvvv@yandex.ru>
2023-01-28 16:34:20 +0300

    StatsService edits

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -3,12 +3,11 @@ package ru.netology.stats;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-
 public class StatsServiceTest {
 
     @Test
     public void ShouldSum() {
-        long sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         StatsService service = new StatsService();
 
         long actual = service.sum(sales);
@@ -16,26 +15,56 @@ public class StatsServiceTest {
         Assertions.assertEquals(expected, actual);
     }
 
-    {
-        @Test
-        public void ShouldSum () {
-        long sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        StatsService service = new StatsService();
-
-        long actual = service.sum(sales);
-        long expected = 180 / 12;
-        Assertions.assertEquals(expected, actual)
-    }
-    }
-
-
     @Test
-    public void ShouldSum() {
-        long sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+    public void Shouldaverage() {
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         StatsService service = new StatsService();
 
-        int actual = service.belowAverege(sales);
+        long actual = service.average(sales);
         long expected = 15;
         Assertions.assertEquals(expected, actual);
 
     }
+
+    @Test
+    public void ShouldMinMonth() {
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        StatsService service = new StatsService();
+
+        int actual = service.minSales(sales);
+        long expected = 9;
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void ShouldMaxMonth() {
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        StatsService service = new StatsService();
+
+        int actual = service.maxSales(sales);
+        long expected = 8;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void SholdbelowAverege() {
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        StatsService service = new StatsService();
+
+        int actual = service.belowAverege(sales);
+        long expected = 5;
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void SholdAboveAverege() {
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        StatsService service = new StatsService();
+
+        int actual = service.AboveAverege(sales);
+        long expected = 5;
+        Assertions.assertEquals(expected, actual);
+    }
+}
